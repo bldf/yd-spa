7bd101df71ac4f07b4f216fb97157fe504fb7dd1
bldf <bldf@qq.com>
2018-12-25 18:31:37 +0800

    提交

--- src/router.js
+++ src/router.js
@@ -35,6 +35,12 @@ const router = new Router({
         // UserProfile 会被渲染在 User 的 <router-view> 中
         path: '/materiel/mt',
         component: () => import("./components/views/module/tabs.vue")
+      },
+      {
+        // 当 /user/:id/profile 匹配成功，
+        // UserProfile 会被渲染在 User 的 <router-view> 中
+        path: '/materiel/mtp',
+        component: () => import("./components/views/module/pageTabs.vue")
       }
     //   {
     //     // 当 /user/:id/posts 匹配成功
